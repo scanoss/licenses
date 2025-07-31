d3aa4a7a237febfa63ea6a375d2ee485aa4edbe7
Agustin Isasmendi <agustin.isasmendi@scanoss.com>
2025-07-31 16:42:27 +0200

    feat: get license from DB

--- pkg/usecase/license_usecase.go
+++ pkg/usecase/license_usecase.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"context"
 	"errors"
+	purlutils "github.com/scanoss/go-purl-helper/pkg"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/scanoss/go-models/pkg/scanoss"
@@ -46,27 +48,73 @@ func NewLicenseUseCaseWithLicenseModel(config *myconfig.ServerConfig, licModel m
 
 // GetLicenses
 func (lu LicenseUseCase) GetLicenses(ctx context.Context, s *zap.SugaredLogger, sc *scanoss.Client, components []dto.ComponentRequestDTO) ([]*pb.ComponentLicenseInfo, *Error) {
+	var cli []*pb.ComponentLicenseInfo
 
-	for _, c := range components {
-		_, _ = sc.Component.GetComponent(types.ComponentRequest{
-			Purl:        c.Purl,
-			Requirement: c.Requirement,
+	// Get a component version. It may be specified on the purl, on the requirement, or the request may not have specified a version at all.
+	for _, component := range components {
+		c, err := sc.Component.GetComponent(types.ComponentRequest{ //TODO: Maybe create a new method that returns a packageurl.PackageURL type?
+			Purl:        component.Purl,
+			Requirement: component.Requirement,
+		})
+
+		if err != nil {
+			s.Warnf("error when resolving component version. %w", err)
+			continue
+		}
+
+		p, err := purlutils.PurlFromString(c.Purl)
+		if err != nil {
+			s.Warnf("error parsing purl: %s. %w", c.Purl, err)
+		}
+
+		urls, err := sc.Models.AllUrls.GetURLsByPurlNameTypeVersion(p.Name, p.Type, c.Version)
+		if err != nil {
+			s.Warnf("cannot get url from purl: %s. %w", c.Purl, err)
+			continue
+		}
+
+		if len(urls) == 0 {
+			s.Warnf("no license found for purl=%s - version=%s. %w", c.Purl, c.Version)
+			continue
+		}
+
+		// Asume that all URLs have same license. One URL is one .tar.gz on a Github Release for example
+		// Sometimes a purl@version have multiple urls (because multiple sources released)
+		// TODO: In order to solve this mapping issue we need to mine licenses based on purl@version
+		//		There is an intent to solve this mapping with the table ldb_component_licenses but there are so many missing licenses
+		lId := urls[0].LicenseID
+
+		l, err := sc.Models.Licenses.GetLicenseByID(lId)
+
+		// l.LicenseName: contains the license as declared from the project. There are scenarios where a project may declare multiple licenses.
+		//					usually is done with SPDX expressions, but some projects declare csv of licenses, or separated by slash
+
+		// l.LicenseID: This is the SPDX version of the license name, if the project have multiple licenses then, each is separated by a "/"
+
+		// There are others scenarios where the project does not declare a specific string license name. and instead have multiple licenses
+		// that apply based on the component compiled or component used. Those cases are not considerer here, like FFmpeg/FFmpeg
+
+		spdxIDs := strings.Split(l.LicenseID, "/")
+
+		// Convert spdxIDs array to []*pb.LicenseInfo
+		var licenses []*pb.LicenseInfo
+		for _, spdxID := range spdxIDs {
+			licenses = append(licenses, &pb.LicenseInfo{
+				Id:       strings.TrimSpace(spdxID),
+				FullName: "", // TODO: Implement SPDX ID to full name mapping
+			})
+		}
+
+		cli = append(cli, &pb.ComponentLicenseInfo{
+			Purl:      c.Purl,
+			Version:   c.Version,
+			Statement: l.LicenseName,
+			Licenses:  licenses,
 		})
 
 	}
 
-	var a []*pb.ComponentLicenseInfo
-
-	a = append(a, &pb.ComponentLicenseInfo{
-		Purl:      "example",
-		Version:   "v1.0.0",
-		Statement: "GPL",
-		Licenses: []*pb.LicenseInfo{
-			{Id: "GPL v2", FullName: "General Public Licence V2"},
-		},
-	})
-
-	return a, nil
+	return cli, nil
 }
 
 // GetDetails
